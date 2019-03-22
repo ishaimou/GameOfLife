63d22d7df202a61cdce07ddcb609fb1d400666c9
Ilyas Azar <ilyassukun@gmail.com>
2019-03-22 22:37:10 +0000

    graphic is loading

--- gol_algo.lisp
+++ gol_algo.lisp
@@ -1,25 +1,26 @@
-(defparameter w 7)
-(defparameter h 7)
+(defparameter row 200)
+(defparameter column 200)
 (defparameter matrix nil)
 (defparameter density nil)
 (defparameter nb 0)
 
-;(setq matrix (make-array (list w h) :initial-element 0))
-
+(setq matrix (make-array (list row column) :initial-element 0))
+#|
 (setq matrix #2A(
- (0 0 0 0 0 0 0)
- (0 0 0 1 0 0 0)
- (0 0 0 0 1 0 0)
- (0 0 1 1 1 0 0)
- (0 0 0 0 0 0 0)
- (0 0 0 0 0 0 0)
- (0 0 0 0 0 0 0)))
-
-(setq density (make-array (list w h) :initial-element 0))
+ (0 0 0 0 0 0 0 0)
+ (0 0 0 1 0 0 0 0)
+ (0 0 0 0 1 0 0 0)
+ (0 0 1 1 1 0 0 0)
+ (0 0 0 0 0 0 0 0)
+ (0 0 0 0 0 0 0 0)
+ (0 0 0 0 0 0 0 0)
+ (0 0 0 0 0 0 0 0)))
+|#
+(setq density (make-array (list row column) :initial-element 0))
 
 (defun random-matrix ()
-  (dotimes (i h)
-    (dotimes (j w)
+  (dotimes (i row)
+    (dotimes (j column)
       (setf (aref matrix i j) (random 2))))
   matrix)
 
@@ -27,21 +28,21 @@
   (equal (aref matrix i j) 1))
 
 (defun is-neighbor (i j)
-  (if (AND (>= i 0) (< i h) (>= j 0) (< j w))
+  (if (AND (>= i 0) (< i row) (>= j 0) (< j column))
       (if (equal (aref matrix i j) 1) 1 0) 0))
 
-(defun get-neighbors (row column)
+(defun get-neighbors (x y)
   (setf nb 0)
-  (loop for i from (- row 1) to (+ row 1)
-        do (loop for j from (- column 1) to (+ column 1)
+  (loop for i from (- x 1) to (+ x 1)
+        do (loop for j from (- y 1) to (+ y 1)
                  do (setf nb (+ nb (is-neighbor i j)))))
-  (if (equal (aref matrix row column) 1)
+  (if (equal (aref matrix x y) 1)
       (decf nb))
   nb)
 
 (defun calculate-density ()
-  (dotimes (i h)
-    (dotimes (j w)
+  (dotimes (i row)
+    (dotimes (j column)
       (setf (aref density i j) (get-neighbors i j))))
   density)
 
@@ -58,25 +59,23 @@
   (if (AND (NOT(is-alive i j)) (equal (aref density i j) 3)) t nil))
 
 (defun update-matrix ()
-  (dotimes (i h)
-    (dotimes (j h)
+  (dotimes (i row)
+    (dotimes (j column)
       (if (rule1 i j) (setf (aref matrix i j) 0))
       (if (rule2 i j) (setf (aref matrix i j) 1))
       (if (rule3 i j) (setf (aref matrix i j) 0))
       (if (rule4 i j) (setf (aref matrix i j) 1))))
   matrix)
 
-;(setq matrix (random-matrix))
-
-#|
-(dotimes (x 20)
-    (print matrix)
-    (format t "Generation ~d~%" (1+ x))
-    (setq density (calculate-density))
-    (setq matrix (update-matrix)))
-|#
+(setq matrix (random-matrix))
 
 (defun gol-algo ()
  (progn
    (setq density (calculate-density))
    (setq matrix (update-matrix))))
+#|
+(dotimes (x 20)
+    (print matrix)
+    (format t "Generation ~d~%" (1+ x))
+    (gol-algo))
+|#
--- sdltest.lisp
+++ sdltest.lisp
@@ -1,13 +1,28 @@
 (ql:quickload :lispbuilder-sdl
               :silent t)
-(defparameter width 500)
-(defparameter height 500)
-(defparameter bsize 15)
+(defparameter width 800)
+(defparameter height 600)
+(defparameter bsize 10)
 (defparameter *white* sdl:*white*)
 (load "gol_algo.lisp")
 
-(defun print-box (x y size color)
-    (sdl:draw-box (sdl:rectangle-from-midpoint-* x y size size) :color color))
+
+(defun print-box (x y bsize color)
+    (sdl:draw-box (sdl:rectangle-from-midpoint-*
+                    (+ x (floor bsize 2)) 
+                    (+ y (floor bsize 2)) bsize bsize)
+                  :color color))
+
+(defun print-matrix (matrix row column bsize)
+  (sdl:clear-display sdl:*black*)
+  (dotimes (i row)
+    (dotimes (j column)
+      (if (equal (aref matrix i j) 1)
+          (print-box (+ 1 (* i bsize))
+                     (+ 1 (* j bsize))
+                     (- bsize (floor bsize 10))
+                     *white*))))
+  (sdl:update-display))
 
 (defun gol-launcher ()
   (sdl:with-init ()
@@ -16,8 +31,13 @@
                              :double-buffer t
                              :title-caption "Carnifex: Game Of Life")
                  (setf (sdl:frame-rate) 60)
+                 ; Enable the keyboard repeat rate setting DELAY and INTERVAL to default values of SDL
+                 (sdl:enable-key-repeat nil nil)
                  (sdl:with-events ()
                                   (:quit-event () t)
+                                  ;; Redraw the screen when it has been modified outside (e.g window manager)
+                                  (:video-expose-event ()
+                                   (sdl:update-display))
                                   (:key-down-event ()
                                    (when (sdl:key-down-p :sdl-key-escape)
                                      (sdl:push-quit-event)))
@@ -28,5 +48,10 @@
                                    ;; Draw the box having a center at the mouse x/y coordinates
                                     (print-box (sdl:mouse-x) (sdl:mouse-y) bsize *white*))
                                    ;; Redraw the display
-                                    (sdl:update-display)))))
+                                    ;(sdl:update-display)
+                                    ;(print (sdl:key-repeat-delay))
+                                    ;(print (sdl:key-repeat-interval))
+                                    (print-matrix matrix row column bsize)
+                                    (gol-algo)
+                                    ))))
 (gol-launcher)
